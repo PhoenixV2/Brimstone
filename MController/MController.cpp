b5de9adf0393bb8492f9ebb7fab5870a6f03bb01
Andrew <andrew@black.ai>
2021-10-29 13:04:43 +1300

    Initial Commit

---
+++ MController/MController.cpp
@@ -0,0 +1,184 @@
+#include "MController.h"
+
+MController::MController(int dirPin, int stepPin){
+	_step_pin = stepPin;
+	_dir_pin = dirPin;
+	_direction = HIGH; // Default to clockwise
+	pinMode(_step_pin, OUTPUT);
+	pinMode(_dir_pin, OUTPUT);
+
+	_min_pulse_width = 10; // (6 for A4988) Microseconds
+	_step_interval = 0;
+	_prev_step_time = 0;
+	_v_time = 0;
+	_step_count = 0;
+	_eStop = false;
+
+	// Default Values
+	_max_velocity = ARDUINO_MAX_V;
+	_velocity = 0;
+	_acceleration = 100; //20
+	_a = 0;
+	_jerk = 25; //2
+
+	set_motor_zero();
+}
+
+// IMPLEMENT THIS PROPERLY
+void MController::run(){
+	// TODO: 
+	if (!_eStop){
+		long steps_to_go = steps_remaining();
+		float new_v = calc_new_velocity(steps_to_go);
+		// TODO: 
+	
+	}
+}
+
+bool MController::run_velocity(){
+	// if "_step_interval == 0" then no motion should occur
+	if(_step_interval){
+		_curr_time = micros();
+		// Check if the _step_interval has elapsed since the previous
+		// step
+		if(_curr_time - _prev_step_time >= _step_interval){
+			if(_direction){
+				_step_count++;
+			}else{
+				_step_count--;
+			}
+			_prev_step_time = _curr_time;
+			step();
+		}
+		return true;
+	}
+	return false;
+}
+
+long MController::steps_remaining(){
+	return _step_count - _new_position;
+}
+
+float MController::calc_new_velocity(float V){
+
+	// Get time step
+	_T_TEMP = micros();
+	float dt = (_T_TEMP - _v_time)/1000000.0; // Scale it back to seconds
+	_v_time = _T_TEMP;
+	// Serial.println(dt);
+	if(V > _max_velocity){
+		V = _max_velocity;
+	}
+	if(abs(V - _velocity) > _v_error){
+		float nV = 0.5*_a*_a/_jerk;
+		
+		// Get the sign of the difference between current and target V
+		int c = ((V - _velocity) > 0) - ((V - _velocity) < 0);
+
+		if(abs(V - _velocity) <= nV){
+			if(abs(_a) > 0){
+				_a += -c*_jerk*dt; 
+			}
+		}else if(abs(_a) < _acceleration){
+			_a += c*_jerk*dt;
+		}
+	}else{
+		// We have reached the target velocity
+		_a = 0;
+		_velocity = V;
+	}
+	_velocity += _a*dt; 
+	// Set direction, if V is positive -> clockwise
+	// if V is negative -> anticlockwise
+	_direction = (_velocity >= 0)?HIGH:LOW; 
+	return _velocity;
+}
+
+void MController::set_speed(float targetV){
+	// Calculate the pulse width for the specified steps/s
+	if(targetV <= ARDUINO_MAX_V){
+		_velocity = targetV;
+	}else{
+		_velocity = ARDUINO_MAX_V;
+	}
+	_step_interval = 1000000.0/abs(_velocity); // µs per step
+}
+
+void MController::calc_interval(){
+	if(_velocity >= _max_velocity){
+		_velocity = _max_velocity;
+	}
+	if(abs(_velocity) > 0){
+		_step_interval = 1000000.0/abs(_velocity); // µs per step
+	}else{
+		_step_interval = 0;
+	}
+}
+
+void MController::set_motor_zero(){
+	_step_count = 0;
+}
+
+void MController::reset_eStop(){
+	_eStop = false;
+}
+
+void MController::set_eStop(int eStopPin){
+	_eStop_pin = eStopPin;
+	pinMode(eStopPin, INPUT);
+	_eStop = false;
+}
+
+void MController::set_max_velocity(float maxV){
+	if(maxV < ARDUINO_MAX_V){
+		_max_velocity = maxV;
+	}else{
+		_max_velocity = ARDUINO_MAX_V;
+	}
+}
+
+void MController::set_acceleration(float maxA){
+	_acceleration = abs(maxA);
+}
+
+void MController::set_on_pulse(int microseconds){
+	_min_pulse_width = abs(microseconds);
+}
+
+void MController::step(){
+	digitalWrite(_dir_pin, _direction);
+	digitalWrite(_step_pin, HIGH);
+	// Wait minimum pulse width <- This is blocking
+	// TODO: Find a non-blocking method for this step procedure for controllers with high clock speeds
+	delayMicroseconds(_min_pulse_width);
+	digitalWrite(_step_pin, LOW);
+}
+
+
+/**
+ * USER UTILITY FUNCTIONS 
+ * The following functions do not aid in the running of the motor controller
+ * but can be used for debugging
+ */
+
+void MController::print_speed(float targetV){
+	
+	Serial.print(_velocity);
+	Serial.print("\t");
+	Serial.print(_a);
+	Serial.print("\t");
+	Serial.print(_step_count);
+	Serial.print("\t");
+	Serial.println(targetV);
+}
+
+void MController::print_headers(){
+	Serial.print("Velocity");
+	Serial.print("\tAcceleration");
+	Serial.print("\tPosition");
+	Serial.println("\tTarget velocity");
+}
+
+float MController::get_velocity(){
+	return _velocity;
+}
---
+++ MController/MController.h
@@ -0,0 +1,105 @@
+#ifndef MController_h
+#define MController_h
+
+#include <stdlib.h>
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#include <wiring.h>
+#endif
+
+#define ARDUINO_MAX_V 3000 // Steps per second
+
+class MController{
+	public:
+		MController(){}
+		MController(int dirPin, int stepPin);
+		
+		// Runs the motor linearly from the current position to the new position at max velocity
+		// Stops at new position with V = 0
+		void run();
+		// Triggers a step with velocity spacing set by "_step_interval"
+		bool run_velocity();
+		// Sets the motor "_velocity" and "_step_interval" so that the motor can run
+		// at a velocity indefinitely by calling run_v()
+		void set_speed(float targetV);
+		// Sets the maximum velocity in steps per second
+		void set_max_velocity(float maxV);
+		// Sets the maximum acceleration in steps per second^2
+		void set_acceleration(float maxA);
+		// Sets the minimum on time in µs for the motor controller to register a step command
+		void set_on_pulse(int microseconds);
+		// Sets the current position of the motor to be zero - resets the step counter (_step_count)
+		void set_motor_zero();
+		
+		// Sets the input pin to trigger the emergency stop
+		// Ideally place on an interrupt pin
+		// TODO: Manage eStop interrupts
+		void set_eStop(int eStopPin);
+		void reset_eStop();
+
+		// Prints the current speed of the motor and the last time it stepped
+		void print_speed(float targetV);
+		void print_headers();
+
+		void calc_interval();
+		float calc_new_velocity(float V);
+		float get_velocity();
+
+	private:
+		// Triggers a step pulse to the motor controller
+		void step();
+		// Calculates the new velocity for the next step to reach the target velocity
+		// incorporating jerk
+		// float calc_new_velocity(float V);
+		// Returns the number of steps from the current position to the _new_position
+		long steps_remaining();
+
+	private:
+		// Direction pin of the motor controller
+		int _dir_pin;
+		// Step pin of the motor controller
+		int _step_pin;
+		// Sets the direction of the motors operation: HIGH or LOW
+		bool _direction;
+
+		// Current speed of the motor
+		float _velocity;
+		// Maximum velocity
+		float _max_velocity;
+		// Current acceleration
+		float _a;
+		// Maximum acceleration
+		float _acceleration;
+		// Max Jerk
+		int _jerk;
+		// v calc time
+		unsigned long _v_time;
+		float _v_error = 0.001; //0.0001
+		float _p_error = 0.001;
+		
+		// Pulse width is defined by the minimum pulse width to trigger the step signal (µs)
+		long _min_pulse_width; 
+		// Time between steps required to achieve the set velocity (µs)
+		float _step_interval;
+		// Time in µs that the previous step was taken
+		unsigned long _prev_step_time;
+		// Current time - assigning here to save memory
+		unsigned long _curr_time;
+
+		// For storing temporay time values
+		unsigned long _T_TEMP;
+
+		// Total number of steps taken by the motor from its zeroed position
+		long int _step_count;
+		// Step count of the new position to be moved to
+		long _new_position;
+
+		// Flag to emergency stop the motors motion in its current position
+		volatile bool _eStop;
+		int _eStop_pin;
+};
+
+
+#endif
